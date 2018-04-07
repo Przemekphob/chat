3bc96410c3dfdeddd787a159619ee95db25fa362
Przemek Mazur <Przemek1713@gmail.com>
2018-04-07 11:40:43 +0200

    index.html, MessageForm.js, MessageList.js added

--- index.js
+++ index.js
@@ -16,35 +16,35 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-	socket.on('join', function(name) {
-		userService.addUser({
-			id: socket.id,
-			name
-		});
+	  socket.on('join', function(name) {
+		    userService.addUser({
+    			 id: socket.id,
+    			 name
+		    });
 
 		io.emit('update', {
-			users: userService.getAllUsers()
+			 users: userService.getAllUsers()
 		});
 
-	});
+	 });
 
   	socket.on('disconnect', () => {
-    	userService.removeUser(socket.id);
-    	socket.broadcast.emit('update', {
-      		users: userService.getAllUsers()
-    	});
+    	  userService.removeUser(socket.id);
+    	  socket.broadcast.emit('update', {
+      	    users: userService.getAllUsers()
+    	  });
   	});
 
   	socket.on('message', function(message){
-    	const {name} = userService.getUserById(socket.id);
-    	socket.broadcast.emit('message', {
-      		text: message.text,
-      		from: name
-    	});
+    	  const {name} = userService.getUserById(socket.id);
+    	  socket.broadcast.emit('message', {
+      		  text: message.text,
+      		  from: name
+    	  });
   	});
 
 });
 
 server.listen(3000, function() {
-	console.log('listening on *:3000');
+	  console.log('listening on *:3000');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 const env = process.env.NODE_ENV || 'development';
-const plugins = [];
+const plugins = [
+    new HtmlWebpackPlugin ({
+        template: './client/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin(),
+        new OptimizeJsPlugin({
+            sourceMap: false
+        })
+    );
+}
 
 module.exports = {
     entry: './client/index.js',
@@ -40,30 +57,6 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        ...plugins,
-        new HtmlWebpackPlugin({
-            template: './client/index.html',
-            filename: 'index.html',
-            inject: 'body'
-        }), 
-        new webpack.optimize.UglifyJsPlugin(),
-        new OptimizeJsPlugin({
-            sourceMap: false
-        })      
-    ]
+    plugins
 };
 
-
-
-
-console.log('NODE_ENV:', env);
-
-if (env === 'production') {
-plugins.push(
-    new webpack.optimize.UglifyJsPlugin(),
-    new OptimizeJsPlugin({
-      sourceMap: false
-    })
-  );
-}
